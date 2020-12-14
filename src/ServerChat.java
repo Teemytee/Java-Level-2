29cebe52243f13b06e668865f14004825de90a42
Teemytee <44222851+Teemytee@users.noreply.github.com>
2020-12-14 19:30:58 +0300

    lesson 7

---
+++ src/ServerChat.java
@@ -0,0 +1,84 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServerChat implements Chat {
+    private ServerSocket serverSocket;
+    private Set<ClientHandler> clients;
+    private AuthenticationService authenticationService;
+
+    public ServerChat() {
+        start();
+    }
+
+    @Override
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+    private void start() {
+        try {
+            serverSocket = new ServerSocket(8888);
+            clients = new HashSet<>();
+            authenticationService = new AuthenticationService();
+
+            while (true) {
+                System.out.println("Server is waiting for a connection ...");
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket, this);
+                System.out.println(String.format("[%s] Client[%s] is successfully logged in", new Date(), clientHandler.getName()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void broadcastMessage(String message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    public void privateMessage(StringBuilder message, String receiver, String sender){
+        for (ClientHandler client : clients) {
+            if (client.getName().equals(receiver)){
+                client.sendMessage(String.format("[%s] sent you a private message: %s", sender, message));
+            }
+        }
+    }
+
+    @Override
+    public void errorMessage(int errorNumber, String nickname) {
+        if (errorNumber == 1){
+            for (ClientHandler client : clients) {
+                if (client.getName().equals(nickname)){
+                    client.sendMessage("You cannot send messages to yourself");
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isNicknameOccupied(String nickname) {
+        for (ClientHandler client : clients) {
+            if (client.getName().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    @Override
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+}
